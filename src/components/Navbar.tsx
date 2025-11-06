79439b18795cfb1d8414664cce15f4c185dfbbe6
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-11-06 10:02:40 +0100

    feat(navbar): make logo clickable by wrapping with Link component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { HiMenuAlt3 } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
@@ -10,10 +11,12 @@ const Navbar = () => {
 
   return (
     <nav className="h-[var(--navbar-height)] bg-white flex items-center justify-between px-5 lg:px-10 w-screen sticky top-0 z-50">
-      <div className="flex items-center gap-[10px]">
-        <img src="/logo.svg" alt="logo" width={40} height={40} />
-        <p className="text-navy-blue text-[18px] font-bold">SwapXnext</p>
-      </div>
+      <Link to="/">
+        <div className="flex items-center gap-[10px]">
+          <img src="/logo.svg" alt="logo" width={40} height={40} />
+          <p className="text-navy-blue text-[18px] font-bold">SwapXnext</p>
+        </div>
+      </Link>
 
       <ul className="text-[16px] items-center gap-[30px] hidden lg:flex">
         {links.map((link) => (
